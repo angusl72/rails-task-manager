9afd08842d8f33f52d4b1aeb2a1d91c26045cbd6
Angus Laird <anguslaird72@gmail.com>
2022-11-10 20:10:17 +1100

    fin

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to task_path(task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to task_path(task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  # strong params, enables us to validate specific data before it's passed through to our database.
+  # ensures people can't create new fields and update things in our DB.
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
